2bdce4f43a4feb05a521973af053971e62265368
Janeleq <janelim2001@gmail.com>
2023-06-11 23:40:00 +0800

    Update Files

--- CS-INSPIRE/src/pages/AgeismRoleplayPage.tsx
+++ CS-INSPIRE/src/pages/AgeismRoleplayPage.tsx
@@ -3,7 +3,7 @@ import React, {useState, useEffect} from 'react';
 import NavigationBar from '../components/NavBarLogin'
 import { NavLink, useNavigate } from 'react-router-dom'
 import Footer from '../components/Footer';
-import action from '../assets/action.png'
+import actionIcon from '../assets/action.png'
 import reflectionIcon from '../assets/reflectionIcon.png'
 import background from '../assets/ageism.svg'
 import roleplay from '../assets/roleplaying.jpg'
@@ -47,10 +47,10 @@ function Ageism() {
         <div className="container-fluid bootdey p-0" style={{overflow: 'hidden'}}>
             <NavigationBar/>
             <div className="row" style={{marginTop: '9vh'}}>
-                <div className='col-sm-12 col-md-4 col-lg-6' style={{backgroundImage:`url(${background})`, backgroundSize: 'cover', height: '100vh'}}>
+                <div className='col-sm-12 col-md-4 col-lg-6' style={{backgroundImage:`url(${background})`, backgroundSize: 'cover', height: '120vh'}}>
 
                 </div>
-                <div className="col-sm-12 col-md-8 col-lg-6 text-center" style={{height: '100vh'}}>
+                <div className="col-sm-12 col-md-8 col-lg-6 text-center bg-white" style={{height: '120vh'}}>
                     <div className="section-title mt-5">
                         <h4 className="title mb-4">Unconscious Bias: <mark>Ageism</mark></h4>
                         <p className="text-muted para-desc mx-auto mb-0">
@@ -59,49 +59,64 @@ function Ageism() {
                         We recognize that ageism can have far-reaching consequences in society, affecting employment opportunities, healthcare access, and interpersonal relationships.
                         </p>
                         <br/><br/>
-                        <div className='row mb-5'>
-                          <div className='col'>
-                            <p className='lead bg-light w-25 mx-auto' style={{fontWeight: 'bold'}}>Roleplay</p>
-                          
-                            <small>
-                              There are a few prompts that will appear in the roleplay.
-                            </small>
-               
-                            <div   className='guide mx-auto'>
-                              <div className='row'>
-                                <div className='col'>
-                                  <img src={action} alt="action" style={{width: '20%'}}/><br/>
-                                  <small className=''>carry out an action</small>
-                                </div>
-                                <div className='col'>
-                                  <img src={reflectionIcon} alt="reflection" style={{width: '20%'}}/><br/>
-                                  <small className=''>write down your thoughts</small>
-                                </div>
-                                <div className='col'>
-                                  <img src={outcomeIcon} alt="outcome" style={{width: '20%'}}/><br/>
-                                  <small className=''>view the consequence of your action</small>
-                                </div>
-                              </div>
-                                <br/>
-                                <button className='mx-auto text-center' onClick={AgeismRoleplay}>Start Roleplay</button>
-                            </div> 
-                          </div>
-                        </div>
-                        <br/>
                         <div className='row'>
-                          <div className='col'>
-                              <p className='lead bg-light w-25 mx-auto' style={{fontWeight: 'bold'}}>Statistics</p>
-                              
-                              <div className='guide w-50 mx-auto'>
-                                <p>Get to know some statistics regarding ageism!</p>
-                              
-                                  
-                                
-                                  <i className="fa fa-male" aria-hidden="true"></i>
-                                  <button type="" onClick={AgeismFacts}>View statistics</button>
-                              </div> 
-                            </div>
+                  <div className='col'>
+                    <p className='lead bg-light w-25 mx-auto' style={{fontWeight: 'bold'}}>Roleplay</p>
+                  
+                    <p className='mb-0'>Take up a roleplay to learn and explore more about Ageism!</p>
+                      
+                    <small className="">
+                      There are a few prompts that will appear in the roleplay.
+                    </small>
+                    
+       
+                    <div className='guide mx-auto text-muted mt-3'>
+                      <div className='row'>
+                        <div className='col'>
+                          <img src={actionIcon} alt="action" style={{width: '20%'}}/><br/>
+                          <small className=''>carry out an action</small>
                         </div>
+                        <div className='col'>
+                          <img src={reflectionIcon} alt="reflection" style={{width: '20%'}}/><br/>
+                          <small className=''>write down your thoughts</small>
+                        </div>
+                        <div className='col'>
+                          <img src={outcomeIcon} alt="outcome" style={{width: '20%'}}/><br/>
+                          <small className=''>view the consequence of your action</small>
+                        </div>
+                      </div>
+                        <br/>
+                        <button className='mx-auto text-center' onClick={AgeismRoleplay}>Start Roleplay</button>
+                    </div> 
+                  </div>
+                </div>
+                <br/><br/>
+                <div className='row'>
+                  <div className='col'>
+                      <p className='lead bg-light w-25 mx-auto' style={{fontWeight: 'bold'}}>Statistics</p>
+                      
+                      <div className='guide w-50 mx-auto'>
+                        <p>Get to know some statistics regarding Ageism!</p>
+                        <i className="fa fa-male" aria-hidden="true"></i>
+                        <button type="" onClick={AgeismFacts}>View statistics</button>
+                      </div> 
+                    </div>
+                </div>
+                <br/><br/>
+                <div className='row'>
+                  <div className='col'>
+                      <p className='lead bg-light w-25 mx-auto' style={{fontWeight: 'bold'}}>Quizzes</p>
+                      
+                      <div className='guide w-50 mx-auto'>
+                        <p>Take some quizzes regarding Ageism!</p>
+                      
+                          
+                        
+                          <i className="fa fa-male" aria-hidden="true"></i>
+                          <button type="" onClick="">View quizzes</button>
+                      </div> 
+                    </div>
+                </div>
                  
                       
                     </div>
